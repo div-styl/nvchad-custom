92dcb525c93015e5abcd2f6428b9993d242afacd
div-styl <gouhmada@gmail.com>
2023-05-30 12:39:12 +0100

    nvchad

---
+++ README.md
@@ -0,0 +1,2 @@
+# NVchad custom
+
---
+++ custom/plugins.lua
@@ -0,0 +1,114 @@
+local overrides = require("custom.configs.overrides")
+
+---@type NvPluginSpec[]
+local plugins = {
+
+  -- Override plugin definition options
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
+
+  -- override plugin configs
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  -- Install a plugin
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+
+  -- To make a plugin not be loaded
+  -- {
+  --   "NvChad/nvim-colorizer.lua",
+  --   enabled = false
+  -- },
+
+  -- All NvChad plugins are lazy-loaded by default
+  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+  -- {
+  --   "mg979/vim-visual-multi",
+  --   lazy = false,
+  -- }
+    {
+    "anuvyklack/pretty-fold.nvim",
+    event = "BufWinEnter",
+    dependencies = {
+      {
+        "anuvyklack/fold-preview.nvim",
+        dependencies = {
+          "anuvyklack/keymap-amend.nvim",
+        },
+        opts = {
+          border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+        },
+      },
+    },
+    config = function()
+      require "custom.configs.external.pretty-fold"
+    end,
+  },
+    {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+      "kevinhwang91/promise-async",
+      {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+          require("custom.configs.external.nvim-ufo").statuscolSetup()
+        end,
+      },
+    },
+    event = "BufReadPost",
+    keys = require("custom.configs.external.nvim-ufo").ufoKeys,
+    config = function()
+      require("custom.configs.external.nvim-ufo").ufoSetup()
+    end,
+  },
+  {
+    "shellRaining/hlchunk.nvim",
+    event = "BufReadPost",
+    config = function()
+      require "custom.configs.external.hlchunk"
+    end,
+  },
+  {
+    "shellRaining/hlchunk.nvim",
+    event = "BufReadPost",
+    config = function()
+      require "custom.configs.external.hlchunk"
+    end,
+  },
+}
+
+return plugins
